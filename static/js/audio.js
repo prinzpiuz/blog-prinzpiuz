5c1953048a1dfb6ad3b7f30887d6d4eb81e118f1
prinzpiuz <prinzpiuz@gmail.in>
2024-03-29 12:00:12 +0530

    feat(reviews): audio wave animation in post

--- static/js/audio.js
+++ static/js/audio.js
@@ -2,16 +2,19 @@ const player = document.getElementById("music-player");
 player.volume = 0.2;
 const audio_on_button = document.getElementById("audi_on");
 const audio_off_button = document.getElementById("audio_off");
+const audio_wave = document.getElementById("audio_wave");
 
 audio_on = () => {
   player.volume = 0.0;
   audio_on_button.style.display = "none";
   audio_off_button.style.display = "inline";
+  audio_wave.style.display = "none";
 };
 audio_off = () => {
   player.volume = 0.2;
   audio_on_button.style.display = "inline";
   audio_off_button.style.display = "none";
+  audio_wave.style.display = "inline";
 };
 
 audio_on_button.addEventListener("click", audio_on);
